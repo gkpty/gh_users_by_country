528c37654bed21ec6230684f319c1f1770766a78
gkpty <gkardonski@gmail.com>
2020-10-01 03:22:11 -0500

    made changes to puppet script

--- puppet.js
+++ puppet.js
@@ -1,15 +1,18 @@
 const puppeteer = require('puppeteer-core');
 const fs = require('fs')
-
+const path = require('path')
+const homeDir = require('os').homedir();
+const homePath = path.join(path.relative(__dirname, '/'), homeDir)
 
 module.exports = async function followUsers(start, limit){
   if(!start) start=0
   if(!limit) limit=10
+  let followed = 0
   let users = JSON.parse(fs.readFileSync('./users.json', 'utf8'))
   const browser = await puppeteer.launch({ 
     executablePath: '/usr/bin/chromium-browser',
     headless: false,
-    userDataDir: '~/.config/chromium'
+    userDataDir: path.join(homePath, '~/.config/chromium')
   });
   let follow_button = '[class*="css-18t94o4 css-1dbjc4n r-1niwhzg r-p1n3y5 r-sdzlij r-1phboty r-rs99b7 r-1w2pmg r-1vuscfd r-1dhvaqw r-1ny4l3l r-1fneopy r-o7ynqc r-6416eg r-lrvibr"]'
   try {
@@ -20,8 +23,9 @@ module.exports = async function followUsers(start, limit){
       let link = `https://twitter.com/${users[ids[i]].username}`;
       try {
         await page.goto(link);
-        await page.waitForSelector(follow_button, {timeout: 1000})
+        await page.waitForSelector(follow_button, {timeout: 1200})
         await page.click(follow_button);
+        followed+=1
         console.log('followed '+ users[ids[i]].username)
       } catch (error) {
         console.log(`User ${users[ids[i]].username} doesnt exist in twitter or is already being followed`)
@@ -29,6 +33,7 @@ module.exports = async function followUsers(start, limit){
     }
     await page.close();
     await browser.close();
+    return `\x1b[32mDone! followed ${followed}/${limit} users\x1b[0m`
   }
   catch (error) {
     console.log(error);
