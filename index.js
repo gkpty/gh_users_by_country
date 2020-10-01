d46ca1decd4c97a43746b2bbe68602579d3180f5
gkpty <gkardonski@gmail.com>
2020-10-01 00:11:10 -0500

    added stuff for sending mass mail

--- index.js
+++ index.js
@@ -74,10 +74,15 @@ function jsonToCsv(){
 
 //send mass mail to users with email
 async function sendMassMail(source){
-  let user_arr = fs.readFileSync('users.json', 'utf8')
-  let users = JSON.parse(user_arr)
+  //let user_arr = fs.readFileSync('users.json', 'utf8')
+  //let users = JSON.parse(user_arr)
+  let users = {
+    lucas:{name:'lucas kardonski', email:'lucas@torus-digital.com'},
+    gabriel:{name:'gabriel kardonski', email:'gkardonski@gmail.com'},
+    joespeh:{name:'lucas kardonski', email:'lucas.kardonski@gmail.com'}
+  }
   let mail_users = new Array
-  for(let u of users) if(u.email) mail_users.push(u)
+  for(let u in users) if(u.email) mail_users.push(u)
   let destinations = new Array
   for(let m of mail_users){
     destinations.push({
@@ -91,9 +96,9 @@ async function sendMassMail(source){
   }
   let template = {
     TemplateName: "gh_users_by_country",
-    SubjectPart: "Greetings, {{name}}!",
-    HtmlPart: "<h1>Hello {{name}},</h1><p>Your favorite animal is {{favoriteanimal}}.</p>",
-    TextPart: "Dear {{name}},\r\nYour favorite animal is {{favoriteanimal}}."
+    SubjectPart: "Hola {{name}}, estás invitadx a Hacktoberfest Panamá 2020",
+    HtmlPart: `<h2>Hola {{name}},</h2><br><p>Me encontré con tu perfil en Github y pensé que te pudiera interesar un evento que estamos organizando para Hacktoberfest, este domingo 4 de octubre 2020 a las 2:00PM.</p> <br><p>Vamos a estar hablando sobre git, colaborar en <b>open-source</b>, y cómo aprender <b>nuevos lenguajes y frameworks</b> más fácil. La idea es promocionar la participación en proyectos de open-source y aportar a crear una comunidad nueva e inclusiva de desarrolladores panameñxs.</p><br><p>Si esto es algo que te interesa, puedes registrarte al evento a través del siguiente <a href=”https://organize.mlh.io/participants/events/4765-hacktoberfest-en-espanol-panama”>enlace</a></p><br><p>Te cuento también que vamos a estar organizando una sección de demos de 3-5 minutos al final del evento. Si tienes algún proyecto open-source que te interesaría mostrar, puedes responderme a este correo con el link al proyecto y una explicación y empezamos a coordinar.</p><br>Nos vemos en Zoom: <a href="https://us02web.zoom.us/j/81910711595?pwd=U1dLY2xTNU82c1AyZTlwZE1OWmZaZz09">https://us02web.zoom.us/j/81910711595?pwd=U1dLY2xTNU82c1AyZTlwZE1OWmZaZz09</a><Agregar a tu calendario aquí>Saludos,Lucas<img src=”https://s3.us-east-1.amazonaws.com/panamadevhub.com/hf_panama_email.png”>`,
+    TextPart: `Hola {{name}},\nMe encontré con tu perfil en Github y pensé que te pudiera interesar un evento que estamos organizando para Hacktoberfest, este domingo 4 de octubre 2020 a las 2:00PM. \nVamos a estar hablando sobre git, colaborar en open-source, y cómo aprender nuevos lenguajes y frameworks más fácil. La idea es promocionar la participación en proyectos de open-source y aportar a crear una comunidad nueva e inclusiva de desarrolladores panameñxs.\nSi esto es algo que te interesa, puedes registrarte al evento a través del siguiente enlace: \nhttps://organize.mlh.io/participants/events/4765-hacktoberfest-en-espanol-panama\nTe cuento también que vamos a estar organizando una sección de demos de 3-5 minutos al final del evento. Si tienes algún proyecto open-source que te interesaría mostrar, puedes responderme a este correo con el link al proyecto y una explicación y empezamos a coordinar.\nNos vemos en Zoom: https://us02web.zoom.us/j/81910711595?pwd=U1dLY2xTNU82c1AyZTlwZE1OWmZaZz09\n<Agregar a tu calendario aquí>\nSaludos,\nLucas\n`
   }
   await ses.createTemplate(template).promise()
   var params = {
@@ -102,7 +107,7 @@ async function sendMassMail(source){
     ReplyToAddresses: [
       source,
     ],
-    Destinations: []
+    Destinations: destinations
   };
   ses.sendBulkTemplatedEmail(params).promise()
   .then(()=> console.log('All Done!')).catch(err=>console.log(err))
